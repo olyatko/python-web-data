cf134a8eefaee6b92d3f9324a4a88b6da5771a43
Olga <charminola@yahoo.com>
2016-11-02 18:50:33 +0200

    Create w6_assign_6_2.py

---
+++ w6_assign_6_2.py
@@ -0,0 +1,17 @@
+import urllib
+import json
+
+# serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+serviceurl = 'http://python-data.dr-chuck.net/geojson?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+    js = json.loads(str(data))
+    print js["results"][0]["place_id"]
